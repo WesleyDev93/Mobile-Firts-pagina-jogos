b2cc6c449c4489bf0b104f78993ac8d3d8d0a981
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-03 18:07:46 -0300

    Alteraçao de css e adição do primeiro arquivo da vida de JS

---
+++ testebotaoJS/testecomprar.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+const botaoComprar = document.getElementById ('botaocomprar');
+
+botaoComprar.addEventListener('click', function(){
+
+    alert("Obrigado pela sua compra!")
+})
+
+
+
+const botaoEnviar = document.getElementById ('botaoenviar');
+
+botaoEnviar.addEventListener('click', function(){
+
+    alert("Sua Mensagem foi enviada com sucesso!")
+})
